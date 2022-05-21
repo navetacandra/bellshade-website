0d8885dccc51f6339ce00a120810f7564493d1a0
navetacandra <naveta.cand@gmail.com>
2022-05-21 23:54:48 +0700

    Add some language image

---
+++ pages/learn/dart/index.jsx
@@ -0,0 +1,14 @@
+import Layout from "@/components/Template/Layout";
+import WrapperTemplate from "@/components/Template/WrapperTemplate";
+
+export default function Page() {
+  return (
+    <Layout pageTitle="Bellshade | Dart">
+      <WrapperTemplate>
+        <div className="my-5 flex justify-center">
+          <h2 className="mb-5 title-2">Coming Soon!</h2>
+        </div>
+      </WrapperTemplate>
+    </Layout>
+  );
+}
--- pages/learn/index.jsx
+++ pages/learn/index.jsx
@@ -1,44 +1,28 @@
 import Link from "next/link";
 import Layout from "@/components/Template/Layout";
-import JavascriptSVG from "@/components/svg-image/JavascriptSVG";
-import JavaSVG from "@/components/svg-image/JavaSVG";
+import dataJson from "@/data-json/learn.json";
 
 export default function Page() {
   return (
     <Layout pageTitle="Bellshade | Learn">
-      <div className="container mx-auto">
+      <div className="container mx-auto mb-10">
         <section className="my-5 grid grid-cols-1 md:grid-cols-2 gap-4">
-          <Link href="/learn/javascript">
-            <div className="inline-flex gap-4 p-4 shadow-md bg-white dark:bg-slate-800 rounded-md transition-all hover:shadow-lg dark:hover:bg-slate-700 ease-out duration-300 cursor-pointer">
-              <div>
-                <JavascriptSVG />
+          {dataJson.map((data, i) => (
+            <Link href={data.link} key={`${i}-${data.id}`}>
+              <div className="inline-flex gap-4 p-4 shadow-md bg-white dark:bg-slate-800 rounded-md transition-all hover:shadow-lg dark:hover:bg-slate-700 ease-out duration-300 cursor-pointer">
+                <div className="shrink-0 h-16 w-16 md:h-32 md:w-32 my-auto">
+                  <picture className="inline-block w-full h-full">
+                    <img src={data.img} alt={`${data.lang}'s Icon`} />
+                  </picture>
+                </div>
+                <div className="my-auto">
+                  <h3 className="font-Semibold text-3xl line-clamp-1 dark:text-slate-50">
+                    Tutorial {data.lang}
+                  </h3>
+                </div>
               </div>
-              <div>
-                <h3 className="font-Semibold text-3xl line-clamp-1 my-1 dark:text-slate-50">
-                  Tutorial Dasar Javascript
-                </h3>
-                <p className="text-sm line-clamp-2 text-slate-700 dark:text-slate-300">
-                  Pelajari dasar-dasar pemrograman Javascript untuk membuat
-                  website yang interaktif.
-                </p>
-              </div>
-            </div>
-          </Link>
-          <Link href="/learn/java">
-            <div className="inline-flex gap-4 p-4 shadow-md bg-white dark:bg-slate-800 rounded-md transition-all hover:shadow-lg dark:hover:bg-slate-700 ease-out duration-300 cursor-pointer">
-              <div>
-                <JavaSVG />
-              </div>
-              <div>
-                <h3 className="font-Semibold text-3xl line-clamp-1 my-1 dark:text-slate-50">
-                  Tutorial Dasar Java
-                </h3>
-                <p className="text-sm line-clamp-2 text-slate-700 dark:text-slate-300">
-                  Pelajari dasar-dasar pemrograman Java dari nol.
-                </p>
-              </div>
-            </div>
-          </Link>
+            </Link>
+          ))}
         </section>
       </div>
     </Layout>
