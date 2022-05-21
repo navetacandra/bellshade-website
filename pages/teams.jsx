166fb4949fbeb26251a52684162584d1e0ceed1c
navetacandra <naveta.cand@gmail.com>
2022-05-21 22:48:30 +0700

    Change teams data to static json

--- pages/teams.jsx
+++ pages/teams.jsx
@@ -1,9 +1,8 @@
 import Image from "next/image";
-import Layout from "../components/Template/Layout";
+import Layout from "@/components/Template/Layout";
+import Members from "@/data-json/bellshade-member.json";
 
-export default function Page(props) {
-  const { Members } = props;
-
+export default function Page() {
   return (
     <Layout pageTitle="Bellshade | Teams">
       <section className="w-full m-auto" style={{ width: "90%" }}>
@@ -14,52 +13,38 @@ export default function Page(props) {
         </div>
         <div className="container mx-auto mb-10">
           <div className="grid grid-cols-1 md:grid-cols-2 gap-4 mx-auto my-5">
-            {Members.map((val, i) =>
-              val ? (
-                <a
-                  href={`https://github.com/${val.username}`}
-                  target="_blank"
-                  rel="noreferrer"
-                  key={i}
-                >
-                  <div className="inline-flex gap-4 p-4 w-full shadow-md bg-white dark:bg-slate-800 rounded-md transition-all hover:shadow-lg dark:hover:bg-slate-700 cursor-pointer">
-                    <div className="mx-1 relative max-w-max tooltip top max-h-16 my-auto">
-                      <div className="relative flex justify-start items-center rounded-full overflow-hidden w-16 h-16 shadow-lg">
-                        <Image
-                          src={`https://res.cloudinary.com/demo/image/fetch/${val.avatar}`}
-                          alt={`${val.username}'s Profile Pict`}
-                          layout="fill"
-                          className="w-full rounded-full"
-                        />
-                      </div>
-                    </div>
-                    <div className="pt-1 w-full mx-7">
-                      <span className="name md:text-lg font-semibold whitespace-normal text-dark dark:text-slate-300">
-                        {val.name}
-                      </span>
-                      <span className="name my-1 md:text-md font-semibold whitespace-normal text-slate-800 dark:text-slate-300 block">
-                        {val.username}
-                      </span>
+            {Members.map((val, i) => (
+              <a
+                href={`https://github.com/${val.username}`}
+                target="_blank"
+                rel="noreferrer"
+                key={i}
+              >
+                <div className="inline-flex gap-4 p-4 w-full shadow-md bg-white dark:bg-slate-800 rounded-md transition-all hover:shadow-lg dark:hover:bg-slate-700 cursor-pointer">
+                  <div className="mx-1 relative max-w-max tooltip top max-h-16 my-auto">
+                    <div className="relative flex justify-start items-center rounded-full overflow-hidden w-16 h-16 shadow-lg">
+                      <Image
+                        src={`https://res.cloudinary.com/demo/image/fetch/${val.avatar}`}
+                        alt={`${val.username}'s Profile Pict`}
+                        layout="fill"
+                        className="w-full rounded-full"
+                      />
                     </div>
                   </div>
-                </a>
-              ) : null,
-            )}
+                  <div className="pt-1 w-full mx-7">
+                    <span className="name md:text-lg font-semibold whitespace-normal text-dark dark:text-slate-300">
+                      {val.name}
+                    </span>
+                    <span className="name my-1 md:text-md font-semibold whitespace-normal text-slate-800 dark:text-slate-300 block">
+                      {val.username}
+                    </span>
+                  </div>
+                </div>
+              </a>
+            ))}
           </div>
         </div>
       </section>
     </Layout>
   );
-}
-
-export async function getServerSideProps() {
-  const MembersData = await (
-    await fetch(`${process.env.PUBLIC_URI}/api/member`)
-  ).json();
-  return {
-    props: {
-      Members: MembersData.members || [],
-      MembersCount: MembersData.members.length || 0,
-    },
-  };
 }
